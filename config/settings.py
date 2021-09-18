402979fa044dbb4646be3b837ae3d86bf69b8d88
Frank Fernandez <frankfernogando@gmail.com>
2021-09-18 12:08:46 +0200

    add posgres db

--- config/settings.py
+++ config/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'mqlsky0^+gl461e2%xlf-#b=b@ci4=$j=4b&h=i)8c^)_)b6re'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['instagram-clonef.herokuapp.com']
 
 
 # Application definition
@@ -99,10 +99,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
+DATABASES['default'] = env.db('DATABASE_URL')  # NOQA
 
 
 # Password validation
